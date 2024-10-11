a13f55d60ac9def05646813eede9bac3de86d78e
Benjamin Good <benjamingood@google.com>
2024-10-11 14:08:28 -0600

    Adding provider metadata to the modules. (#17)

--- reactive-governance/alert/deploy/main.tf
+++ reactive-governance/alert/deploy/main.tf
@@ -16,6 +16,12 @@ Copyright 2024 Google LLC
 
 ## RESOURCES TO SETUP ALERTING WHEN RESOURCE IS CREATED OR UPDATED AND LABEL IS MISSING ##
 
+terraform {
+  provider_meta "google" {
+    module_name = "cloud-solutions/cost-attribution-solution-alert-v1.1"
+  }
+}
+
 locals {
   expanded_region = var.region == "us-central" || var.region == "europe-west" ? "${var.region}1" : var.region
 }
