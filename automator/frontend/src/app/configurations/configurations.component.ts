941106c0a6fc56f9be70a1e2b10a63ea24fded3e
Ricardo Godoy <rgodoy@google.com>
2025-03-13 19:40:34 -0300

    Adding first configuration component.

--- automator/frontend/src/app/configurations/configurations.component.ts
+++ automator/frontend/src/app/configurations/configurations.component.ts
@@ -1,10 +1,23 @@
 import { Component } from "@angular/core";
+import { FormControl, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
 
 @Component({
   selector: "app-configurations",
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: "./configurations.component.html",
   styleUrl: "./configurations.component.scss",
 })
-export class ConfigurationsComponent {}
+export class ConfigurationsComponent {
+  chosenAssetTypes = new FormControl("");
+  assetTypes: string[] = ["Project", "Compute Engine", "Big Query"];
+}
